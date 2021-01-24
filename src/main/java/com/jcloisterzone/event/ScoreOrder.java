16e0e363533b7da819012bd991e5465da5475b94
stanolacko <stano.lacko@gmail.com>
2021-01-24 09:37:19 +0100

    Preparation for ScoreOrder sort

---
+++ src/main/java/com/jcloisterzone/event/ScoreOrder.java
@@ -0,0 +1,10 @@
+package com.jcloisterzone.event;
+
+import com.jcloisterzone.Immutable;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Set;
+import io.vavr.collection.Function;
+
+@Immutable
+public class ScoreOrder {
+}
