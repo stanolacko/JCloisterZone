55e76a2b5cf51fd13b31fd2dcd835a6933d63bf5
stanolacko <stano.lacko@gmail.com>
2026-07-01 19:50:42 +0200

    Windows rundev allows to set "-v" argument

--- scripts/rundev.bat
+++ scripts/rundev.bat
@@ -1,3 +1,6 @@
 @Echo off
 
-npm install && npm run build && node dist\cli\jcz-engine.js -p 9001
+set "EXTRA="
+if /I "%~1"=="-v" set "EXTRA=-v"
+
+npm install && npm run build && node dist\cli\jcz-engine.js -p 9001 %EXTRA%
